4897130d6d348ffa49f082a7b3f42e979d89a097
jrsmith12138 <849070287@qq.com>
2020-12-19 14:00:03 +0800

    fix

--- goodsservice/src/test/java/cn/edu/xmu/goods/controller/PresaleControllerTest.java
+++ goodsservice/src/test/java/cn/edu/xmu/goods/controller/PresaleControllerTest.java
@@ -2,7 +2,8 @@ package cn.edu.xmu.goods.controller;
 
 import cn.edu.xmu.goods.GoodsServiceApplication;
 import cn.edu.xmu.goods.service.PresaleService;
-import org.junit.Test;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -11,6 +12,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -23,8 +25,6 @@ public class PresaleControllerTest
     @Autowired
     private MockMvc mvc;
 
-    @MockBean
-    private PresaleService presaleService;
 /*
     public void createPresale() throws Exception{
         String token = null;
@@ -80,9 +80,21 @@ public class PresaleControllerTest
                 "}";
         JSONAssert.assertEquals(expectedResponse, responseString, true);
     }
- */
+
+        @Test
+    public void getPresaleGoodsNot() throws Exception{
+        String responseString = this.mvc.perform(
+                put("/presales")
+                        .queryParam("skuId","99"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andReturn().getResponse().getContentAsString();
+
+        String expectedResponse = "{\"errno\":504,\"errmsg\":\"操作的资源id不存在\"}";
+        JSONAssert.assertEquals(expectedResponse, responseString, true);
+    }
     @Test
-    public void createPresaleGoodsError() throws Exception{
+    public void createPresaleGoods() throws Exception{
         String token = null;
 
         String contentJson = "{\n" +
@@ -90,9 +102,9 @@ public class PresaleControllerTest
                 "  \"advancePayPrice\": 250,\n" +
                 "  \"restPayPrice\": 350,\n" +
                 "  \"quantity\": 100,\n" +
-                "  \"beginTime\": \"2020-12-12T05:16:59\",\n" +
-                "  \"payTime\": \"2021-12-12T05:16:59\",\n" +
-                "  \"endTime\": \"2021-06-12T05:16:59\"\n" +
+                "  \"beginTime\": \"2021-12-12T05:16:59\",\n" +
+                "  \"payTime\": \"2022-12-12T05:16:59\",\n" +
+                "  \"endTime\": \"2022-06-12T05:16:59\"\n" +
                 "}";
 
         String responseString = this.mvc.perform(
@@ -106,16 +118,17 @@ public class PresaleControllerTest
         String expectedResponse = "{\"errno\":505,\"errmsg\":\"操作的资源id不是自己的对象\"}";
         JSONAssert.assertEquals(expectedResponse, responseString, true);
     }
+ */
+
     @Test
-    public void getPresaleGoodsNot() throws Exception{
-        String responseString = this.mvc.perform(
-                put("/presales")
-                        .queryParam("skuId","99"))
-                .andExpect(status().isNotFound())
+    public void getpresaleStates() throws Exception{
+        String responseString=this.mvc.perform(get("/presales/states"))
+                .andExpect(status().isOk())
                 .andExpect(content().contentType("application/json;charset=UTF-8"))
                 .andReturn().getResponse().getContentAsString();
-
-        String expectedResponse = "{\"errno\":504,\"errmsg\":\"操作的资源id不存在\"}";
+        System.out.println(responseString);
+        String expectedResponse = "{\"errno\": 0, \"data\": [{ \"name\": \"已下线\", \"code\": 0 },{ \"name\": \"已上线\", \"code\": 1 },{ \"name\": \"已删除\", \"code\": 2 }],\"errmsg\": \"成功\"}";
         JSONAssert.assertEquals(expectedResponse, responseString, true);
     }
+
 }
