96b4102750f5088bdb040c4566b4018f79d4f842
firstday1 <2602164122@qq.com>
2020-12-19 17:57:42 +0800

    sql

--- goodsservice/src/main/resources/goods-schema.sql
+++ goodsservice/src/main/resources/goods-schema.sql
@@ -1,19 +1,184 @@
 -- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
 --
--- Host: localhost    Database: oomall_goods
+-- Host: localhost    Database: oomall
 -- ------------------------------------------------------
 -- Server version	8.0.22
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `auth_new_user`
+--
+
+DROP TABLE IF EXISTS `auth_new_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `auth_new_user` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(32) DEFAULT NULL,
+  `mobile` varchar(128) DEFAULT NULL,
+  `email` varchar(128) DEFAULT NULL,
+  `name` varchar(32) DEFAULT NULL,
+  `avatar` varchar(255) DEFAULT NULL,
+  `open_id` varchar(128) DEFAULT NULL,
+  `depart_id` bigint DEFAULT NULL,
+  `gmt_create` datetime DEFAULT NULL,
+  `password` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `auth_new_user_user_name_uindex` (`user_name`),
+  UNIQUE KEY `auth_new_user_mobile_uindex` (`mobile`),
+  UNIQUE KEY `auth_new_user_email_uindex` (`email`),
+  UNIQUE KEY `auth_new_user_open_id_uindex` (`open_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=23336 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `auth_privilege`
+--
+
+DROP TABLE IF EXISTS `auth_privilege`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `auth_privilege` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) DEFAULT NULL,
+  `url` varchar(512) DEFAULT NULL,
+  `request_type` tinyint DEFAULT NULL,
+  `signature` varchar(256) DEFAULT NULL,
+  `gmt_create` datetime DEFAULT NULL,
+  `gmt_modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1001 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `auth_role`
+--
+
+DROP TABLE IF EXISTS `auth_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `auth_role` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL COMMENT '角色名称',
+  `creator_id` bigint NOT NULL COMMENT '创建者',
+  `descr` varchar(500) DEFAULT NULL COMMENT '角色描述',
+  `gmt_create` datetime NOT NULL COMMENT '创建时间',
+  `gmt_modified` datetime DEFAULT NULL COMMENT '修改时间',
+  `depart_id` bigint NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `auth_role_name_uindex` (`name`),
+  KEY `auth_role_creator_id_index` (`creator_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=778 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='角色表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `auth_role_privilege`
+--
+
+DROP TABLE IF EXISTS `auth_role_privilege`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `auth_role_privilege` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `role_id` bigint DEFAULT NULL,
+  `privilege_id` bigint DEFAULT NULL,
+  `creator_id` bigint DEFAULT NULL,
+  `gmt_create` datetime DEFAULT NULL,
+  `signature` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `auth_role_privilege_role_id_index` (`role_id`),
+  KEY `auth_role_privilege_creator_id_index` (`creator_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=138 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `auth_user`
+--
+
+DROP TABLE IF EXISTS `auth_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `auth_user` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(32) NOT NULL COMMENT '用户名',
+  `password` varchar(128) NOT NULL,
+  `mobile` varchar(128) DEFAULT NULL,
+  `mobile_verified` tinyint DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `email_verified` tinyint DEFAULT NULL,
+  `name` varchar(128) DEFAULT NULL COMMENT 'y用户真实姓名',
+  `avatar` varchar(255) DEFAULT NULL COMMENT '头像',
+  `last_login_time` datetime DEFAULT NULL,
+  `last_login_ip` varchar(63) DEFAULT NULL,
+  `open_id` varchar(128) DEFAULT NULL COMMENT '用于第三方登录',
+  `state` tinyint DEFAULT NULL COMMENT '用户状态',
+  `depart_id` bigint DEFAULT NULL COMMENT '用户部门，0 平台',
+  `gmt_create` datetime NOT NULL,
+  `gmt_modified` datetime DEFAULT NULL,
+  `signature` varchar(500) DEFAULT NULL COMMENT '对user_name,password,mobile,email,open_id,depart_id签名',
+  `creator_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `auth_user_user_name_uindex` (`user_name`),
+  UNIQUE KEY `auth_user_open_id_uindex` (`open_id`),
+  UNIQUE KEY `auth_user_mobile_uindex` (`mobile`),
+  UNIQUE KEY `auth_user_email_uindex` (`email`),
+  KEY `auth_user_depart_id_index` (`depart_id`),
+  KEY `auth_user_creator_id_index` (`creator_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17330 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `auth_user_proxy`
+--
+
+DROP TABLE IF EXISTS `auth_user_proxy`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `auth_user_proxy` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `user_a_id` bigint DEFAULT NULL,
+  `user_b_id` bigint DEFAULT NULL,
+  `begin_date` datetime DEFAULT NULL,
+  `end_date` datetime DEFAULT NULL,
+  `gmt_create` datetime DEFAULT NULL,
+  `signature` varchar(256) DEFAULT NULL,
+  `valid` tinyint NOT NULL DEFAULT '1',
+  `depart_id` bigint NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `auth_user_proxy_user_a_id_valid_index` (`user_a_id`,`valid`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `auth_user_role`
+--
+
+DROP TABLE IF EXISTS `auth_user_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `auth_user_role` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `role_id` bigint DEFAULT NULL,
+  `user_id` bigint DEFAULT NULL,
+  `creator_id` bigint DEFAULT NULL,
+  `signature` varchar(256) DEFAULT NULL,
+  `gmt_create` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `auth_user_role_role_id_index` (`role_id`),
+  KEY `auth_user_role_creator_id_index` (`creator_id`),
+  KEY `auth_user_role_user_id_index` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=214 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `brand`
@@ -30,7 +195,7 @@ CREATE TABLE `brand` (
   `gmt_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `gmt_modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=120 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=20124 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -51,7 +216,7 @@ CREATE TABLE `comment` (
   `gmt_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `gmt_modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -73,7 +238,7 @@ CREATE TABLE `coupon` (
   `gmt_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `gmt_modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=20014 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -101,7 +266,7 @@ CREATE TABLE `coupon_activity` (
   `gmt_modified` datetime DEFAULT NULL,
   `quantitiy_type` tinyint DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=20007 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -118,7 +283,7 @@ CREATE TABLE `coupon_sku` (
   `gmt_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `gmt_modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=90003 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -136,7 +301,7 @@ CREATE TABLE `flash_sale` (
   `gmt_modified` datetime DEFAULT NULL,
   `state` tinyint DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -155,7 +320,7 @@ CREATE TABLE `flash_sale_item` (
   `gmt_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `gmt_modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -178,7 +343,7 @@ CREATE TABLE `float_price` (
   `gmt_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `gmt_modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9002 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -195,7 +360,7 @@ CREATE TABLE `goods_category` (
   `gmt_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `gmt_modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=140 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=20142 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -221,7 +386,7 @@ CREATE TABLE `goods_sku` (
   `gmt_modified` datetime DEFAULT NULL,
   `state` tinyint DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=680 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=20682 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -246,7 +411,7 @@ CREATE TABLE `goods_spu` (
   `gmt_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `gmt_modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=680 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=20684 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -268,7 +433,7 @@ CREATE TABLE `groupon_activity` (
   `gmt_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `gmt_modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -293,7 +458,7 @@ CREATE TABLE `presale_activity` (
   `gmt_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `gmt_modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3111 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -310,7 +475,7 @@ CREATE TABLE `shop` (
   `gmt_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `gmt_modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=20002 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -322,4 +487,4 @@ CREATE TABLE `shop` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-12-10 22:36:43
+-- Dump completed on 2020-12-19 17:56:24
