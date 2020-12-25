8dcbd95c880deeedfad2aaa96c4676d45e78a303
Hongxueting <447901323@qq.com>
2020-12-25 12:07:34 +0800

    --story=1003338 --user=洪雪婷 顾客查看品牌信息 https://www.tapd.cn/43305476/s/1002651

--- goodsservice/src/main/java/cn/edu/xmu/goods/controller/GoodsController.java
+++ goodsservice/src/main/java/cn/edu/xmu/goods/controller/GoodsController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.Arrays;
-//
 @RestController
 @RequestMapping(value = "", produces = "application/json;charset=UTF-8")
 public class GoodsController {
